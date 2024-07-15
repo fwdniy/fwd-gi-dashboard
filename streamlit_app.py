3aeb5ecd95cba3aa72ae9c251e74e26bceb75627
fwdniy <119481913+fwdniy@users.noreply.github.com>
2024-07-15 09:43:48 +0000

    Added st auth

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+from st_oauth import st_oauth
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+id = st_oauth(label='Click to login via Okta')
