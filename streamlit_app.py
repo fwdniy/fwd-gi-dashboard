b6774b113b703c945484c48c1431bbbad4ad8a15
fwdniy <119481913+fwdniy@users.noreply.github.com>
2024-07-26 07:39:22 +0000

    remove callback from sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from st_oauth import st_oauth
 st.set_page_config(layout="wide", page_title='Stilson Dashboard', page_icon='styles/fwd_ico.png')
 
 def callback():
+    st.session_stage["pages"] = st.session_stage["pages"][1:]
     st.switch_page("Home")
 
 # Open css file
@@ -11,7 +12,6 @@ st.markdown('<style>' + open('./styles/style.css').read() + '</style>', unsafe_a
 
 id = st_oauth('fwdoauth')
 
-pages = [st.Page("pages/home.py", title="Home"), st.Page("pages/callback.py", title="Callback")]
+st.session_stage["pages"] = [st.Page("pages/callback.py", title="Callback"), st.Page("pages/home.py", title="Home")]
 
-nav = st.navigation(pages)
-nav.run()
+nav = st.navigation(st.session_stage["pages"])
