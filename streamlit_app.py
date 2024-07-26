3bb114dbeddad02d7295b4b42bb1f2b28a0b602c
fwdniy <119481913+fwdniy@users.noreply.github.com>
2024-07-26 07:40:05 +0000

    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from st_oauth import st_oauth
 st.set_page_config(layout="wide", page_title='Stilson Dashboard', page_icon='styles/fwd_ico.png')
 
 def callback():
-    st.session_stage["pages"] = st.session_stage["pages"][1:]
+    st.session_state["pages"] = st.session_state["pages"][1:]
     st.switch_page("Home")
 
 # Open css file
@@ -12,6 +12,6 @@ st.markdown('<style>' + open('./styles/style.css').read() + '</style>', unsafe_a
 
 id = st_oauth('fwdoauth')
 
-st.session_stage["pages"] = [st.Page("pages/callback.py", title="Callback"), st.Page("pages/home.py", title="Home")]
+st.session_state["pages"] = [st.Page("pages/callback.py", title="Callback"), st.Page("pages/home.py", title="Home")]
 
-nav = st.navigation(st.session_stage["pages"])
+nav = st.navigation(st.session_state["pages"])
