0d3d58e878993174667fb68cfa59834171545b5c
fwdniy <119481913+fwdniy@users.noreply.github.com>
2024-08-01 05:09:30 +0000

    hide icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,12 +15,19 @@ else:
     pages = st.session_state["pages"]
 
 if "fwdoauth" in st.secrets:
-    hide_github_icon = """
-    #GithubIcon {
-        visibility: hidden;
-    }
+    st.markdown(
     """
-    st.markdown(hide_github_icon, unsafe_allow_html=True)
+        <style>
+            .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+            .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+            .viewerBadge_text__1JaDK {
+                display: none;
+            }
+        </style>
+    """,
+    unsafe_allow_html=True
+    )
+
     id = st_oauth('fwdoauth')
 
 if ("ST_OAUTH" in st.session_state or "fwdoauth" not in st.secrets) and "callback_removed" not in st.session_state:
