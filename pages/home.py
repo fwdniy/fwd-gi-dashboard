77b6a831ea134a28b6a4822f8fe22f2bf876bf9b
fwdniy <119481913+fwdniy@users.noreply.github.com>
2024-11-06 10:00:31 +0000

    fix: test id token decrypt

--- pages/home.py
+++ pages/home.py
@@ -3,4 +3,31 @@ import streamlit as st
 st.write("# Welcome to the Stilson Dashboard! 👋")
 st.write("For any enhancements or bugs, please contact Nicolas Au-Yeung via Teams or email (nicolas.au.yeung@fwd.com)")
 
-st.write(st.session_state["ST_OAUTH"])
+from cryptography.fernet import Fernet
+import json
+import base64
+
+# Load the key
+def load_key():
+    return open("key.key", "rb").read()
+
+# Decrypt the data
+def decrypt_data(encrypted_data):
+    key = load_key()
+    f = Fernet(key)
+    decrypted_data = f.decrypt(encrypted_data)
+    return decrypted_data.decode()
+
+# Example usage
+encrypted_message = st.session_state["ST_OAUTH"]["access_token"]
+print(decrypt_data(encrypted_message))
+
+id_token = st.session_state["token"]["id_token"]
+# verify the signature is an optional step for security
+payload = id_token.split(".")[1]
+# add padding to the payload if needed
+payload += "=" * (-len(payload) % 4)
+payload = json.loads(base64.b64decode(payload))
+email = payload["email"]
+
+st.write()
