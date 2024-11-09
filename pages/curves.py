51dc84918bba00ad5c59ec0b76e6a73fb3fbac98
fwdniy <119481913+fwdniy@users.noreply.github.com>
2024-11-09 01:46:03 +0000

    fix: refactor curves

--- pages/curves.py
+++ pages/curves.py
@@ -6,22 +6,21 @@ import plotly.graph_objects as go
 import pandas as pd
 import io
 
-def get_curve_data():
-    if "curves_rates_df" in st.session_state or "curve_names_df" in st.session_state:
-        return
-    
-    df = snowflake.query("SELECT curve, valuation_date, tenor, rate, timestamp FROM supp.curve_rates;")
-    df['VALUATION_DATE'] = df['VALUATION_DATE'].dt.date
-
-    st.session_state["curves_rates_df"] = df
-
-    df = snowflake.query("SELECT name, fx, type FROM supp.curve_name WHERE type <> 'requested';")
-    st.session_state["curves_names_df"] = df
-
 def build_curve_filter():
+    def get_curve_data():
+        if "curves_rates_df" in st.session_state or "curve_names_df" in st.session_state:
+            return
+        
+        df = snowflake.query("SELECT curve, valuation_date, tenor, rate, timestamp FROM supp.curve_rates;")
+        df['VALUATION_DATE'] = df['VALUATION_DATE'].dt.date
+
+        st.session_state["curves_rates_df"] = df
+
+        df = snowflake.query("SELECT name, fx, type FROM supp.curve_name WHERE type <> 'requested';")
+        st.session_state["curves_names_df"] = df
+
     get_curve_data()
 
-    df_rates = st.session_state["curves_rates_df"]
     df_names = st.session_state["curves_names_df"]
 
     currencies = sorted(df_names['FX'].unique())
@@ -33,21 +32,6 @@ def build_curve_filter():
     curve_name = st.selectbox("Curve", currency_curves, index=next((i for i, item in enumerate(currency_curves) if 'govt' in item), 0))
 
     return curve_name
-
-with st.expander("Filters", True):
-    with st.spinner('Fetching curve data...'):
-        curve_name = build_curve_filter()
-        filter.build_date_filter(True)
-
-    current_date = st.session_state['Valuation Date']
-
-    if "selected_curves" not in st.session_state:
-        st.session_state["selected_curves"] = []
-
-    if st.button("Add"):
-        name = f"{current_date}/{curve_name}"
-        if name not in st.session_state["selected_curves"]:
-            st.session_state["selected_curves"].append(name)
 
 def map_tenor_to_double(tenors, rates):
     tenor_mapping = {"1m": 1 / 12, "3m": 3 / 12, "6m": 6 / 12}
@@ -67,6 +51,21 @@ def map_tenor_to_double(tenors, rates):
             st.write(f"Unknown tenor name '{tenor}'!")
 
     return (tenor_list, rates_list)
+
+with st.expander("Filters", True):
+    with st.spinner('Fetching curve data...'):
+        curve_name = build_curve_filter()
+        filter.build_date_filter(True)
+
+    current_date = st.session_state['Valuation Date']
+
+    if "selected_curves" not in st.session_state:
+        st.session_state["selected_curves"] = []
+
+    if st.button("Add"):
+        name = f"{current_date}/{curve_name}"
+        if name not in st.session_state["selected_curves"]:
+            st.session_state["selected_curves"].append(name)
 
 if len(st.session_state["selected_curves"]) == 0:
     st.write("Pick a curve!")
@@ -102,8 +101,6 @@ else:
         showlegend=True
     )
 
-    st.plotly_chart(fig)
-
     buffer = io.BytesIO()
 
     with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
@@ -117,6 +114,8 @@ else:
         file_name='curve_rates.xlsx',
         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     )
+
+    st.plotly_chart(fig)
 
     for item in st.session_state["selected_curves"]:
         split_list = item.split('/')
