67e1d605b02343ae8daf2dfd89c86061656b7669
fwdniy <119481913+fwdniy@users.noreply.github.com>
2024-12-23 06:27:53 +0000

    fix: remove github icon again

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,18 @@ from streamlit_js_eval import streamlit_js_eval
 st.set_page_config(layout="wide", page_title='Stilson Dashboard', page_icon='styles/fwd_ico.png')
 streamlit_js_eval(js_expressions="window.innerWidth", key='SCR')
 
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 menu('Home')
 
