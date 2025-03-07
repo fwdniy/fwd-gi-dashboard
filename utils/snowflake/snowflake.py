983406df5af4858ffb628b57c44e403b44fda202
fwdniy <119481913+fwdniy@users.noreply.github.com>
2025-03-07 09:24:03 +0000

    feat: add cache to query_data

--- utils/snowflake/snowflake.py
+++ utils/snowflake/snowflake.py
@@ -2,6 +2,7 @@ import snowflake.connector
 from datetime import datetime
 import pandas as pd
 import streamlit as st
+from streamlit import session_state as ss
 
 def connect_snowflake():
     if "conn" in st.session_state:
@@ -54,16 +55,29 @@ def query(query, sort_columns = []):
     if "conn" not in st.session_state:
         connect_snowflake()
     
-    conn = st.session_state["conn"]
-    cur = conn.cursor()
-    cur.execute(query)
-    df = cur.fetch_pandas_all()
+    def query_data(query_string: str):            
+        if 'sql_statement' in ss and query_string == ss['sql_statement']:
+            return ss['query_df']
+        
+        with st.spinner('Fetching your requested data...'):
+            conn = st.session_state["conn"]
+            cur = conn.cursor()
+            cur.execute(query)
+            df = cur.fetch_pandas_all()
 
-    if sort_columns != []:
-        convert_columns(df, cur).sort_values(by=sort_columns)
-    else:
-        convert_columns(df, cur)
+            if sort_columns != []:
+                convert_columns(df, cur).sort_values(by=sort_columns)
+            else:
+                convert_columns(df, cur)
+            
+            ss['query_df'] = df
 
+        ss['sql_statement'] = query_string
+
+        return df
+    
+    df = query_data(query)
+    
     return df
 
 def non_query(query):
