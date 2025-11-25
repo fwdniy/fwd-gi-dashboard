41260203e8d4aa6f10a9c37e0047094db3f7271a
fwdniy <119481913+fwdniy@users.noreply.github.com>
2025-11-25 09:15:02 +0000

    fix: remove reinvestment functions

--- pages/projector.py
+++ pages/projector.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from interface import initialize
-from pages.projector import build_filters, verify_to_load, load_asset_cashflow_data, build_asset_liability_df, build_visuals, get_liabilities, calculate_reinvestment_rates
+from pages.projector import build_filters, verify_to_load, load_asset_cashflow_data, build_asset_liability_df, build_visuals, get_liabilities
 
 LBUS = ['HK', 'TH', 'JP']
-CASHFLOW_TYPES = {'asset': 'Asset Cashflows', 'prem_cf': 'Premiums', 'g_liab': 'Guaranteed Liabilities', 'ng_liab': 'Non-Guaranteed Liabilities', 'reinv': 'Reinvestment Income', 'net_liab': 'Net Liabilities'}
-CASHFLOW_COLORS = {'asset': '#F3BB90', 'prem_cf': '#B5E6A2', 'g_liab': '#A9A9A9', 'ng_liab': '#F2CEEF', 'reinv': '#A2D2E6', 'net_liab': '#D9A7A7'}
+CASHFLOW_TYPES = {'asset': 'Asset Cashflows', 'prem_cf': 'Premiums', 'g_liab': 'Guaranteed Liabilities', 'ng_liab': 'Non-Guaranteed Liabilities', 'net_liab': 'Net Liabilities'}
+CASHFLOW_COLORS = {'asset': '#F3BB90', 'prem_cf': '#B5E6A2', 'g_liab': '#A9A9A9', 'ng_liab': '#F2CEEF', 'net_liab': '#D9A7A7'}
 
 initialize()
 
@@ -15,7 +15,5 @@ verify_to_load()
 security_df, yearly_df = load_asset_cashflow_data(CASHFLOW_TYPES, LBUS)
 
 yearly_df = build_asset_liability_df(yearly_df, get_liabilities())
-
-calculate_reinvestment_rates(yearly_df, CASHFLOW_TYPES)
     
 build_visuals(yearly_df, CASHFLOW_TYPES, CASHFLOW_COLORS, security_df)
